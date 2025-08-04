62fcb6e804c1bf238b9463f1484e88268b77d846
sklwo210jk <cpzmaos@gmail.com>
2025-08-05 01:08:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ def get_results():
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
